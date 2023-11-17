e7e19228fb45a9c91ce18c1d9494fbce510076a1
Arthur Telles <atelles96@gmail.com>
2023-11-17 10:35:02 -0300

    adjust: Hi-DPI enabled

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1700227725|1036158057";
+const CACHE_VERSION = "1700228061|1372457907";
 const CACHE_PREFIX = "lootbox-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
