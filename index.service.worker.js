011680556c9fe08804c4fa176d5175e1d9555eab
Arthur Telles <atelles96@gmail.com>
2023-11-29 11:18:33 -0300

    feat: New UI elements

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,8 +3,8 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1700247720|1098630039";
-const CACHE_PREFIX = "lootbox-sw-cache-";
+const CACHE_VERSION = "1701267490|2443438300";
+const CACHE_PREFIX = "Lootbox-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
