1427933c5553a377a2a3aa90fcfa7ef7477a083c
Arthur Telles <atelles96@gmail.com>
2023-12-08 12:24:42 -0300

    feat: Christmas theme

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701269036|3988956561";
+const CACHE_VERSION = "1702049012|7296777300";
 const CACHE_PREFIX = "Lootbox-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
